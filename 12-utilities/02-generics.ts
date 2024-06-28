d9e6f758e9d817994d6dd4f411a67a446613a607
Kevin Cunningham <kevin@kevincunningham.co.uk>
2024-06-28 12:10:35 +0100

    exercise

--- 12-utilities/02-generics.ts
+++ 12-utilities/02-generics.ts
@@ -4,7 +4,14 @@
 
 interface EventDetails {
   name: string;
-  location: string;
+  location: {
+    venueDetails: {
+      manager: string;
+      contact_number: number;
+    };
+    capacity: number;
+    parkingQuality: number;
+  };
   date: string;
   isPublic?: boolean;
 }
@@ -32,8 +39,42 @@ type ReadOnlyTodo = {
   readonly [Key in keyof Todo]: Todo[Key];
 };
 
+// map((key)=> ({[key]: Todo[key]}))
+
 type OurReadonly<Type> = {
   readonly [Key in keyof Type]: Type[Key];
 };
 
-type ReadOnlyEvents = OurReadonly<EventDetails>;
+// type Readonly<T> = {
+// 	readonly [P in keyof T]: T[P];
+// };
+
+type ReadOnlyEventDetails = OurReadonly<EventDetails>;
+
+type DeeplyReadonly<Type> = {
+  readonly [Key in keyof Type]: Type[Key] extends string | boolean
+    ? Type[Key]
+    : DeeplyReadonly<Type[Key]>;
+};
+
+type DeeplyReadOnlyEventDetails = DeeplyReadonly<EventDetails>;
+
+// is the type value a string or a boolean?
+// Yes? Return it
+// No? Call DeeplyReadonly on the string value
+
+const newEvent: DeeplyReadOnlyEventDetails = {
+  name: "Glastonbury",
+  location: {
+    venueDetails: {
+      manager: "Farmer",
+      contact_number: 123,
+    },
+    capacity: 100000,
+    parkingQuality: 0,
+  },
+  date: "2024-06-28",
+  isPublic: true,
+};
+
+newEvent.location.venueDetails.manager = "Bob";
